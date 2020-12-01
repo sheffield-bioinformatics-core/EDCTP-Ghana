1ea06075f5c9022fc9bfdd2826fb2f767b5e1bda
mattdmem <matthew.parker@sheffield.ac.uk>
2020-12-01 16:36:40 +0000

    snakemake line issue

--- workflow/bin/setup_for_analysis.sh
+++ workflow/bin/setup_for_analysis.sh
@@ -97,5 +97,4 @@ source /home/sarscov2/miniconda3/etc/profile.d/conda.sh
 
 conda activate sarscov2
 
-snakemake --config user_run_name=${USER_RUN_NAME} --snakefile ~/wc/EDCTP-Ghana/workflow/Snakefile --rerun-incomplete --use-conda --jobs 30 --latency-wait 120 --verbose --printshellcmds --stats snake
-make_stats.json all --cluster 'qsub -V' --cleanup-conda
+snakemake --config user_run_name=${USER_RUN_NAME} --snakefile ~/wc/EDCTP-Ghana/workflow/Snakefile --rerun-incomplete --use-conda --jobs 30 --latency-wait 120 --verbose --printshellcmds --stats snakemake_stats.json all --cluster 'qsub -V' --cleanup-conda
